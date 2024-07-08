4ed5ea1252f241e9ace931cee60896577b18b5d0
Fabrizzio Parrillis <fabrizzioparrillis@gmail.com>
2024-07-08 19:05:46 -0300

    v1-19:05

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ module.exports = {
   reactStrictMode: true,
   env: {
     DB_URI: process.env.DB_URI,
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+    GOOGLE_CLIENT_ID:process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET:process.env.GOOGLE_CLIENT_SECRET,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     API:
       process.env.NODE_ENV === "production"
--- utils/authOptions.js
+++ utils/authOptions.js
@@ -3,64 +3,65 @@ import GoogleProvider from 'next-auth/providers/google';
 import User from '@/models/user';
 import bcrypt from 'bcrypt';
 import dbConnect from '@/utils/dbConnect';
-export const authOptions = {
-    session: {
-        strategy: 'jwt',
-    },
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        }),
-        CredentialsProvider({
-            async authorize(credentials) {
-                await dbConnect();
-                const { email, password } = credentials;
-                const user = await User.findOne({ email });
 
-                if (!user) {
-                    throw new Error('Correo electrónico o contraseña incorrectos');
-                }
-                const isPasswordMatched = await bcrypt.compare(password, user.password);
-                if (!isPasswordMatched) {
-                    throw new Error('Correo electrónico o contraseña incorrectos');
-                }
-                return user;
-            },
-        }),
-    ],
-    callbacks: {
-        async signIn({ user }) {
-            const { email, name, image } = user;
-            await dbConnect();
-            let dbUser = await User.findOne({ email });
-            if (!dbUser) {
-                await User.create({
-                    email,
-                    name,
-                    image,
-                });
-            }
-            return true;
-        },
-        async jwt({ token, user }) {
-            if (user) {
-                const userByEmail = await User.findOne({ email: token.email });
-                if (userByEmail) {
-                    userByEmail.password = undefined;
-                    userByEmail.resetCode = undefined;
-                    token.user = userByEmail;
-                }
-            }
-            return token;
-        },
-        async session({ session, token }) {
-            session.user = token.user;
-            return session;
-        },
+export const authOptions = {
+  session: {
+    strategy: 'jwt',
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      async authorize(credentials) {
+        await dbConnect();
+        const { email, password } = credentials;
+        const user = await User.findOne({ email });
+
+        if (!user) {
+          throw new Error('Correo electrónico o contraseña incorrectos');
+        }
+        const isPasswordMatched = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatched) {
+          throw new Error('Correo electrónico o contraseña incorrectos');
+        }
+        return user;
+      },
+    }),
+  ],
+  callbacks: {
+    async signIn({ user }) {
+      const { email, name, image } = user;
+      await dbConnect();
+      let dbUser = await User.findOne({ email });
+      if (!dbUser) {
+        await User.create({
+          email,
+          name,
+          image,
+        });
+      }
+      return true;
     },
-    secret: process.env.NEXTAUTH_SECRET,
-    pages: {
-        signIn: '/login',
+    async jwt({ token, user }) {
+      if (user) {
+        const userByEmail = await User.findOne({ email: token.email });
+        if (userByEmail) {
+          userByEmail.password = undefined;
+          userByEmail.resetCode = undefined;
+          token.user = userByEmail;
+        }
+      }
+      return token;
     },
+    async session({ session, token }) {
+      session.user = token.user;
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: '/login',
+  },
 };
