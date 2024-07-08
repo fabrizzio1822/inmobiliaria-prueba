6059a20439a914768c71fa6fc8ea35705b0f7fa4
Fabrizzio Parrillis <fabrizzioparrillis@gmail.com>
2024-07-08 20:37:59 -0300

    v1-20:37

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,19 +1,8 @@
-import mongoose from 'mongoose';
-
+import mongoose from 'mongoose'
 const dbConnect = async () => {
-  if (mongoose.connection.readyState >= 1) {
-    return;
-  }
-
-  try {
-    await mongoose.connect(process.env.DB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error.message);
-  }
+    if (mongoose.connection.readyState >= 1){
+        return;
+    }
+    mongoose.connect(process.env.DB_URI);
 };
-
 export default dbConnect;
