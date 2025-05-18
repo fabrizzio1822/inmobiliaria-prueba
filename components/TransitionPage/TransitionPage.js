d40e8029c52ee19e52cc60ff4b7e0d6be9ba266f
Fabrizzio Parrillis <fabrizzioparrillis@gmail.com>
2025-05-18 19:07:50 -0300

    vtokko-18/5-19:07

--- components/TransitionPage/TransitionPage.js
+++ components/TransitionPage/TransitionPage.js
@@ -3,10 +3,10 @@ import { AnimatePresence, motion  } from "framer-motion"
 
 export function TransitionPage() {
     return (
-        <AnimatePresence mode="">
+        <AnimatePresence mode="wait">
             <div>
                 <motion.div
-                    className="fixed top-0 bottom-0 right-full w-screen h-screen z-[1000] bg-main-100"
+                    className="absolute top-0 bottom-0 right-full w-screen h-screen z-[1000] bg-main-100"
                     variants={transitionVariantsPage}
                     initial='initial'
                     animate='animate'
@@ -16,7 +16,7 @@ export function TransitionPage() {
 
                 </motion.div>
                 <motion.div
-                    className="fixed top-0 bottom-0 right-full w-screen h-screen z-[999] bg-main-100/70 opacity-59"
+                    className="absolute top-0 bottom-0 right-full w-screen h-screen z-[999] bg-main-100/70 opacity-59"
                     variants={transitionVariantsPage}
                     initial='initial'
                     animate='animate'
